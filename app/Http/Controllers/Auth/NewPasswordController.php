3c81af32b3d14912f1c2379580bf055745c5bf36
Muhammad Asdar MLD <asdardevs@gmail.com>
2023-07-05 00:30:44 +0800

    button invest

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -22,10 +22,10 @@ class NewPasswordController extends Controller
      */
     public function create(Request $request)
     {
-        return Inertia::render('Auth/ResetPassword', [
-            'email' => $request->email,
-            'token' => $request->route('token'),
-        ]);
+        // return Inertia::render('Auth/ResetPassword', [
+        //     'email' => $request->email,
+        //     'token' => $request->route('token'),
+        // ]);
     }
 
     /**
@@ -38,36 +38,36 @@ class NewPasswordController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'token' => 'required',
-            'email' => 'required|email',
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        // $request->validate([
+        //     'token' => 'required',
+        //     'email' => 'required|email',
+        //     'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        // ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user) use ($request) {
-                $user->forceFill([
-                    'password' => Hash::make($request->password),
-                    'remember_token' => Str::random(60),
-                ])->save();
+        // // Here we will attempt to reset the user's password. If it is successful we
+        // // will update the password on an actual user model and persist it to the
+        // // database. Otherwise we will parse the error and return the response.
+        // $status = Password::reset(
+        //     $request->only('email', 'password', 'password_confirmation', 'token'),
+        //     function ($user) use ($request) {
+        //         $user->forceFill([
+        //             'password' => Hash::make($request->password),
+        //             'remember_token' => Str::random(60),
+        //         ])->save();
 
-                event(new PasswordReset($user));
-            }
-        );
+        //         event(new PasswordReset($user));
+        //     }
+        // );
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
-        if ($status == Password::PASSWORD_RESET) {
-            return redirect()->route('login')->with('status', __($status));
-        }
+        // // If the password was successfully reset, we will redirect the user back to
+        // // the application's home authenticated view. If there is an error we can
+        // // redirect them back to where they came from with their error message.
+        // if ($status == Password::PASSWORD_RESET) {
+        //     return redirect()->route('login')->with('status', __($status));
+        // }
 
-        throw ValidationException::withMessages([
-            'email' => [trans($status)],
-        ]);
+        // throw ValidationException::withMessages([
+        //     'email' => [trans($status)],
+        // ]);
     }
 }
