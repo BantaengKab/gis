0261a5e46719062b6fc81777b353863e7d8b415e
moohrahmat <mooh.rahmat@gmail.com>
2023-07-10 23:49:36 +0800

    cek

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,6 +14,27 @@ class DashboardController extends Controller
 {
     public function index()
     {
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        ])->post('https://splp.layanan.go.id/oauth2/token', [
+            'client_id' => 'zxoPNxYWkLVuWRzynxkW799BnGoa',
+            'client_secret' => 'fh6OdJ7O_atJ6fz5S0LsO_6xLR4a',
+            'grant_type' => 'client_credentials',
+        ]);
+        if ($response->successful()) {
+            // Request was successful, handle the response
+           return $response->json();
+            // Process the response data
+        } else {
+            // Request failed, handle the error
+            $errorCode = $response->status();
+            $errorMessage = $response->body();
+            // Handle the error accordingly
+        }
+
+
+
         $investor = Investor::count();
         $peluang = Peluang::count();
         $rencana = Investasi::sum('rencana');
@@ -24,5 +45,8 @@ class DashboardController extends Controller
             'rencana' => $rencana,
             'realisasi' => $realisasi,
         ]);
+
+
+
     }
 }
