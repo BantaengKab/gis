1cfafad8fab31bca5443435cedb8710c02b26e16
moohrahmat <mooh.rahmat@gmail.com>
2023-07-10 16:46:46 +0800

    dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,65 +1,65 @@
 <script setup>
-import { Head } from '@inertiajs/vue3'
-import { computed, ref, onMounted } from 'vue'
-import { useMainStore } from '@/Stores/main'
+import { Head } from "@inertiajs/vue3";
+import { computed, ref, onMounted } from "vue";
+import { useMainStore } from "@/Stores/main";
 import {
-  mdiAccountMultiple,
-  mdiCartOutline,
-  mdiChartTimelineVariant,
-  mdiFinance,
-  mdiMonitorCellphone,
-  mdiReload,
-  mdiGithub,
-  mdiChartPie,
-  mdiMapMarkerRadius
-} from '@mdi/js'
-import * as chartConfig from '@/Components/Charts/chart.config.js'
-import LineChart from '@/Components/Charts/LineChart.vue'
-import SectionMain from '@/Components/SectionMain.vue'
-import CardBoxWidget from '@/Components/CardBoxWidget.vue'
-import CardBox from '@/Components/CardBox.vue'
-import TableSampleClients from '@/Components/TableSampleClients.vue'
-import NotificationBar from '@/Components/NotificationBar.vue'
-import BaseButton from '@/Components/BaseButton.vue'
-import CardBoxTransaction from '@/Components/CardBoxTransaction.vue'
-import CardBoxClient from '@/Components/CardBoxClient.vue'
-import LayoutAuthenticated from '@/Layouts/LayoutAuthenticated.vue'
-import SectionTitleLineWithButton from '@/Components/SectionTitleLineWithButton.vue'
-import SectionBannerStarOnGitHub from '@/Components/SectionBannerStarOnGitHub.vue'
-const chartData = ref(null)
+    mdiAccountMultiple,
+    mdiCartOutline,
+    mdiChartTimelineVariant,
+    mdiFinance,
+    mdiMonitorCellphone,
+    mdiReload,
+    mdiGithub,
+    mdiChartPie,
+    mdiMapMarkerRadius,
+} from "@mdi/js";
+import * as chartConfig from "@/Components/Charts/chart.config.js";
+import LineChart from "@/Components/Charts/LineChart.vue";
+import SectionMain from "@/Components/SectionMain.vue";
+import CardBoxWidget from "@/Components/CardBoxWidget.vue";
+import CardBox from "@/Components/CardBox.vue";
+import TableSampleClients from "@/Components/TableSampleClients.vue";
+import NotificationBar from "@/Components/NotificationBar.vue";
+import BaseButton from "@/Components/BaseButton.vue";
+import CardBoxTransaction from "@/Components/CardBoxTransaction.vue";
+import CardBoxClient from "@/Components/CardBoxClient.vue";
+import LayoutAuthenticated from "@/Layouts/LayoutAuthenticated.vue";
+import SectionTitleLineWithButton from "@/Components/SectionTitleLineWithButton.vue";
+import SectionBannerStarOnGitHub from "@/Components/SectionBannerStarOnGitHub.vue";
+const chartData = ref(null);
 const fillChartData = () => {
-  chartData.value = chartConfig.sampleChartData()
-}
+    chartData.value = chartConfig.sampleChartData();
+};
 onMounted(() => {
-  fillChartData()
-})
-const mainStore = useMainStore()
+    fillChartData();
+});
+const mainStore = useMainStore();
 
 const props = defineProps({
-  investor: {
-    type: Object,
-    default: () => ({}),
-  },
-  peluang: {
-    type: Object,
-    default: () => ({}),
-  },
-  rencana: {
-    type: Object,
-    default: () => ({}),
-  }
-  realisasi: {
-    type: Object,
-    default: () => ({}),
-  }
-})
+    investor: {
+        type: Object,
+        default: () => ({}),
+    },
+    peluang: {
+        type: Object,
+        default: () => ({}),
+    },
+    rencana: {
+        type: Object,
+        default: () => ({}),
+    },
+    realisasi: {
+        type: Object,
+        default: () => ({}),
+    },
+});
 
 /* Fetch sample data */
-mainStore.fetch('clients')
-mainStore.fetch('history')
+mainStore.fetch("clients");
+mainStore.fetch("history");
 
-const clientBarItems = computed(() => mainStore.clients.slice(0, 4))
-const transactionBarItems = computed(() => mainStore.history)
+const clientBarItems = computed(() => mainStore.clients.slice(0, 4));
+const transactionBarItems = computed(() => mainStore.history);
 </script>
 
 <template>
