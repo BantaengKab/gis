f07417aeb28e2c395a65707833f33f5bf4732ec8
moohrahmat <mooh.rahmat@gmail.com>
2023-07-10 23:58:07 +0800

    cek

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,46 +9,20 @@ use App\Models\Peluang;
 use App\Models\Investor;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/x-www-form-urlencoded',
-        ])->post('https://splp.layanan.go.id/oauth2/token', [
-            'client_id' => 'zxoPNxYWkLVuWRzynxkW799BnGoa',
-            'client_secret' => 'fh6OdJ7O_atJ6fz5S0LsO_6xLR4a',
-            'grant_type' => 'client_credentials',
+        $investor = Investor::count();
+        $peluang = Peluang::count();
+        $rencana = Investasi::sum('rencana');
+        $realisasi = Investasi::sum('realisasi');
+        return Inertia::render('Dashboard', [
+            'peluang' => $peluang,
+            'investor' => $investor,
+            'rencana' => $rencana,
+            'realisasi' => $realisasi,
         ]);
-        $responseData = '';
-        if ($response->successful()) {
-            // Request was successful, handle the response
-            $responseData = $response->json();
-            // Process the response data
-        } else {
-            // Request failed, handle the error
-            $errorCode = $response->status();
-            $errorMessage = $response->body();
-            // Handle the error accordingly
-        }
-
-        return $responseData;
-
-        // $investor = Investor::count();
-        // $peluang = Peluang::count();
-        // $rencana = Investasi::sum('rencana');
-        // $realisasi = Investasi::sum('realisasi');
-        // return Inertia::render('Dashboard', [
-        //     'peluang' => $peluang,
-        //     'investor' => $investor,
-        //     'rencana' => $rencana,
-        //     'realisasi' => $realisasi,
-        // ]);
-
-
-
     }
 }
